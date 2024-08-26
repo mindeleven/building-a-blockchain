f5aba28a110e4d7bd7255c07f9fe9d9d4db26f19
mindeleven <mindeleven@posteo.de>
2024-08-26 13:04:22 +0200

    New poetry project for coding along with chapter 3, blockchains; scaffolding blockchain class

---
+++ ch03_blockchains/basic_blockchain/basic_blockchain/blockchain.py
@@ -0,0 +1,17 @@
+class blockchain(object):
+
+    def __init__(self):
+        self.chain = []
+
+    def new_block(self):
+        # generates a new block and adds it to the chain
+        pass
+
+    @staticmethod
+    def hash(block):
+        # hashes a block
+        pass
+
+    def last_block(self):
+        # gets the latest block of the chain
+        pass
