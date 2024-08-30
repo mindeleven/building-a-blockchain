3dee5db20ab7dd7d40f3adbd7f6e8ffefebff1f7
mindeleven <mindeleven@posteo.de>
2024-08-30 23:26:18 +0200

    Fleshing out the new_block() method, flashing out the hash() method, adding the genesis block in the constructor method

--- ch03_blockchains/basic_blockchain/basic_blockchain/blockchain.py
+++ ch03_blockchains/basic_blockchain/basic_blockchain/blockchain.py
@@ -1,21 +1,48 @@
-class blockchain(object):
+import json
+
+from datetime import datetime
+from hashlib import sha256
+
+class Blockchain(object):
 
     def __init__(self):
         self.chain = []
         self.pending_transactions = []
 
-    def new_block(self):
+        # Create the genesis block
+        print("Creating genesis block")
+        self.new_block
+
+
+    def new_block(self, previous_hash=None):
         # generates a new block and adds it to the chain
-        pass
+        block = {
+            'index': len(self.chain),
+            'timestamp': datetime.utcnow().isoformat(),
+            'transactions': self.pending_transactions,
+            'previous_hash': previous_hash,
+        }
+        # get the hash of this new block, and add it to the block
+        block_hash = self.hash(block)
+        block["hash"] = block_hash
+
+        # reset the list of pending transactions
+        self.pending_transactions = []
+        # add the block to the chain
+        self.chain.append(block)
+
+        print(f"Created block {block['index']}")
+        return block
 
     @staticmethod
     def hash(block):
-        # hashes a block
-        pass
+        # we ensure that the dictionary is sorted or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return sha256(block_string).hexdigest()
 
     def last_block(self):
-        # gets the latest block of the chain
-        pass
+        # gets the latest block of the chain (if there are blocks)
+        return self.chain[-1] if self.chain else None
     
     # adding a primitive unsigned transaction for illustration's sake
     def new_transaction(self, sender, recipient, amount):
