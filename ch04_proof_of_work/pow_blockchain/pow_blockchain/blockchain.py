b684f557a3a04c090e81f5ce8ec8ae43ef17aa45
mindeleven <mindeleven@posteo.de>
2024-09-01 12:28:05 +0200

    Fleshing out the proof_of_work and valid_hash methods: creating a new block with a random nonce, hashing the block to see if it's valid, return the new block if (and when) valid

--- ch04_proof_of_work/pow_blockchain/pow_blockchain/blockchain.py
+++ ch04_proof_of_work/pow_blockchain/pow_blockchain/blockchain.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from datetime import datetime
 from hashlib import sha256
@@ -21,16 +22,22 @@ class Blockchain(object):
 
         # Create the genesis block
         print("Creating genesis block")
-        self.new_block()
+        self.chain.append(self.new_block())
     
     def new_block(self, previous_hash=None):
+
+        if self.last_block():
+            previous_hash = self.last_block()["hash"]
+
         # generates a new block and adds it to the chain
         block = {
             'index': len(self.chain),
             'timestamp': datetime.now().isoformat(), #.utcnow().isoformat(),
             'transactions': self.pending_transactions,
             'previous_hash': previous_hash,
-            'nonce': None,
+            # 'previous_hash': self.last_block["hash"] if self.last_block else None,
+            # 'nonce': None,
+            'nonce': format(random.getrandbits(64), "x"),
         }
         # get the hash of this new block, and add it to the block
         block_hash = self.hash(block)
@@ -39,9 +46,9 @@ class Blockchain(object):
         # reset the list of pending transactions
         self.pending_transactions = []
         # add the block to the chain
-        self.chain.append(block)
+        # self.chain.append(block) # not validated, don't add here, add in proof_of_work()
 
-        print(f"Created block {block['index']}")
+        # print(f"Created block {block['index']}")
         return block
 
     @staticmethod
@@ -64,8 +71,19 @@ class Blockchain(object):
         })
 
     def proof_of_work(self):
-        pass
+        while True:
+            # creating a new block with a random nonce
+            new_block = self.new_block()
+            # hash the block to see if it's valid
+            if self.valid_block(new_block):
+                break # break if valid otherwise repeat step
 
-    def valid_hash(self):
-        pass
+        self.chain.append(new_block)
+        print("Found a new block: ", new_block)
+    
+    # method for validating hashes
+    # previously referred to as valid_hash() in the book
+    def valid_block(block):
+        return block["hash"].startswith("0000")
+        
 
