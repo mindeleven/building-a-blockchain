c1c2217331be65ce165f098a20fb634a9c9fbe56
mindeleven <mindeleven@posteo.de>
2024-09-06 12:21:34 +0200

    Added some explanatory comments

--- ch06_cryptography/cryptography_101/cryptography_101/public_key.py
+++ ch06_cryptography/cryptography_101/cryptography_101/public_key.py
@@ -1,4 +1,14 @@
-# public key cryptography
+# public key cryptography is an example of asymetric cryptography
+# it involves two or more key pairs, one of them being kept secret
+# in this example: the popular RSA algorithm
+# output of all algorithms: a correlated pair of keys A and B
+# these keys can be used for encryting a message and send it through an unsecure channel
+# key A remains secret and private
+# key B is made public
+# (1) public key B is used to encrypt a message that only can be decrypted 
+#     with the correlated private key A
+# (2) private key A can be used to sign a message and public key B can be
+#     used to verify that the message got signed by the correlated private key
 
 from nacl.public import PrivateKey, Box
 import random
@@ -21,6 +31,7 @@ bobs_public_key = bobs_private_key.public_key
 
 # bob will send alice a message
 # -> he makes a box with his private key and alices public key
+# so, the box gets signed with bob's private key and encrypted with alice's public key
 bobs_box = Box(bobs_private_key, alices_public_key)
 
 # we encrypt bob's secret message (bytes)
@@ -28,6 +39,8 @@ encrypted = bobs_box.encrypt(bytes(message[random.randint(0, len(message)-1)], '
 
 # alice creates a second box with her private key 
 # and bob's public key so she can decrypt the message
+# alice verifys that the message is from bob with his public key
+# and decrypts it with her private key
 alices_box = Box(alices_private_key, bobs_public_key)
 
 # now alice can decrypt the message
