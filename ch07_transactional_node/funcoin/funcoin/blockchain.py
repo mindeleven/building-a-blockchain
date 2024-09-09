9db270e039ebc8a91a105c9a2b6df7bff1e887c6
mindeleven <mindeleven@posteo.de>
2024-09-09 13:56:56 +0200

    Some minor changes to get the blockchain running like in chapter 3; so far only temporary state; quite some TODOs

--- ch07_transactional_node/funcoin/funcoin/blockchain.py
+++ ch07_transactional_node/funcoin/funcoin/blockchain.py
@@ -18,7 +18,7 @@ $ python3 -i blockchain.py
 >>> bc = Blockchain()
 >>> bc.chain
 # add a new block by copying the hash from the terminal output
->>> bc.new_block(previous_hash="XXX")
+>>> bc.new_block()
 '''
 
 class Blockchain(object):
@@ -41,15 +41,23 @@ class Blockchain(object):
             target = self.target,
             timestamp = time(),
         )
+        
+        logger.info(f"New block created: {block}")
 
         # reset the list of pending transactions
         self.pending_transactions = []
 
         return block
     
+    # for now it's not really clear how a new block gets appended to the chain
+    # we're adding a helper here
+    # TODO: figuring out where this is supposed to happen
+    def add_a_new_block_to_the_chain(self):
+        self.chain.append(self.new_block())
+
     @staticmethod
     def create_block(
-        self, height, transactions, previous_hash, nonce, target, timestamp=None
+        height, transactions, previous_hash, nonce, target, timestamp=None
     ):
         block = {
             "height": height,
@@ -61,9 +69,12 @@ class Blockchain(object):
         }
 
         # get the hash of this new block and add it to the block
-        block["hash"] = self.hash(block)
+        # block["hash"] = self.hash(block)
+        block_string = json.dumps(block, sort_keys=True).encode()
+        block["hash"] = sha256(block_string).hexdigest()
         return block
-
+    
+    # TODO: is it sufficient the the following method gets replaced with code in create_block?
     @staticmethod
     def hash(block):
         # we ensure that the dictionary is sorted or we'll have inconsistent hashes
@@ -113,6 +124,22 @@ class Blockchain(object):
 
         return self.target
     
+    async def get_blocks_after_timestamp(self, timestamp):
+        for index, block in enumerate(self.chain):
+            if timestamp < block["timestamp"]:
+                return self.chain[index:]
+    
+    async def mine_new_block(self):
+        self.recalculate_target(self.last_block["index"] + 1)
+        while True:
+            new_block = self.new_block()
+            if self.valid_block(new_block):
+                break
+
+            await asyncio.sleep(0)
+        
+        self.chain.append(new_block)
+        logger.info("Found a new block: " + new_block)
     
     # adding a primitive unsigned transaction for illustration's sake
     def new_transaction(self, sender, recipient, amount):
