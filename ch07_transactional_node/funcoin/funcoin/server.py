c6a0237898f95667b33c480eb8d92be227f4a2f1
mindeleven <mindeleven@posteo.de>
2024-09-11 12:59:35 +0200

    Refactoring the copied code from chapter 5

--- ch07_transactional_node/funcoin/funcoin/server.py
+++ ch07_transactional_node/funcoin/funcoin/server.py
@@ -1,72 +1,77 @@
+# here's where our basic TCP Server lives
+import asyncio
+from asyncio import SteamReader, SteamWriter
+
+import structlog
+from marshmallow.exceptions import MarshmallowError
+
+from funcoin.messages import BaseSchema
+from funcoin.utils import get_external_ip
+
+logger = structlog.getLogger()
+
 class Server: 
 
-    def __init__(self) -> None:
+    def __init__(self, blockchain, connection_pool, p2p_protocol):
+        connection_pool = connection_pool
         pass
 
-# minimal chat server example to illustrate how to write a simple 
-# asynchronous TCP server-cliept application in Python
+    async def get_external_ip(self):
+        # finds our "external IP" so we can advertise it to our peers
+        pass
 
-# building the chat server:
-# (1) when user connects prompt them for their nickname
-# (2) broadcast arrival of new user to every connected user (except for new user)
-# (3) broadcast user message to every connected user (except the sender of the msg)
-# (4) predefined messages: 4/1 /list -> show list of all connected users to requesting user
-# (5) predefined messages: 4/2 /quit 
-#     -> disconnect requesting user
-#     -> broadcast "<nick> has quit" to all connected users
-# -> to accomplish tasks we create ConnectionPool class
+    async def handle_connection(self, reader: SteamReader, writer: SteamWriter):
+        # this function is called when we receive a new connection
+        # the writer object represents the connection peer 
 
-import asyncio
-from textwrap import dedent
+        # get a nickname from the client
+        writer.write("> Choose your nickname:\n".encode())
 
-async def handle_connection(reader, writer):
-    # get a nickname from the client
-    writer.write("> Choose your nickname:\n".encode())
+        response = await reader.readuntil(b"\n")
+        writer.nickname = response.decode().strip()
 
-    response = await reader.readuntil(b"\n")
-    writer.nickname = response.decode().strip()
+        # writer.write("You sent: ".encode() + response)
+        self.connection_pool.add_new_user_to_pool(writer)
+        self.connection_pool.send_welcome_message(writer)
+        
+        # announce the arrival of this new user
+        self.connection_pool.broadcast_user_join(writer)
 
-    # writer.write("You sent: ".encode() + response)
-    connection_pool.add_new_user_to_pool(writer)
-    connection_pool.send_welcome_message(writer)
-    
-    # announce the arrival of this new user
-    connection_pool.broadcast_user_join(writer)
+        while True:
+            try:
+                # handle and/or reply to the incoming data
+                data = await reader.readuntil(b"\n")
+            except asyncio.exceptions.IncompleteReadError:
+                # an error happened, break out of the wait loop
+                self.connection_pool.broadcast_user_quit(writer)
+                break
 
-    while True:
-        try:
-            data = await reader.readuntil(b"\n")
-        except asyncio.exceptions.IncompleteReadError:
-            connection_pool.broadcast_user_quit(writer)
-            break
+            message = data.decode().strip()
+            if message == "/quit":
+                self.connection_pool.broadcast_user_quit(writer)
+                break
+            elif message == "/list":
+                self.connection_pool.list_users(writer)
+            else:
+                self.connection_pool.broadcast_new_message(writer, message)
 
-        message = data.decode().strip()
-        if message == "/quit":
-            connection_pool.broadcast_user_quit(writer)
-            break
-        elif message == "/list":
-            connection_pool.list_users(writer)
-        else:
-            connection_pool.broadcast_new_message(writer, message)
+            await writer.drain()
 
-        await writer.drain()
+            if writer.is_closing():
+                break
+        
+        # we're now outside the message loop and the user has quit
 
-        if writer.is_closing():
-            break
-    
-    # we're now outside the message loop and the user has quit
+        # let's close the connection and clean up
+        writer.close()
+        await writer.wait_closed()
 
-    # let's close the connection and clean up
-    writer.close()
-    await writer.wait_closed()
+    async def listen(self, hostname="0.0.0.0", port=8888):
+        # this is the listen method which spwans our server
+        server = await asyncio.start_server(self.handle_connection, hostname, port)
 
+        logger.info(f"Server listening on {hostname}:{port}")
 
-async def main():
-    server = await asyncio.start_server(handle_connection, "127.0.0.1", 8888)
+        async with server:
+            await server.serve_forever()
 
-    async with server:
-        await server.serve_forever()
-
-
-connection_pool = ConnectionPool()
-asyncio.run(main())
