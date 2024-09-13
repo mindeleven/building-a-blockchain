e7e3f82d0d1a5626d457b554696bd84194bb32b5
mindeleven <mindeleven@posteo.de>
2024-09-13 13:04:46 +0200

    Fleshing out new functions and cleaning up not necessary code from initial version

--- ch07_transactional_node/funcoin/funcoin/connections.py
+++ ch07_transactional_node/funcoin/funcoin/connections.py
@@ -1,30 +1,15 @@
 # class to store all the logic that manages our ConnectionPool
 
+import structlog
+from more_itertools import take
+
+logger = structlog.getLogger(__name__)
+
 class ConnectionPool:
 
     def __init__(self):
-        self.connection_pool = set()
-
-    # writer passed to connection pool is an instance of StreamWriter
-    # StreamWriter -> an asyncio object the writes to an underlying connection
-    # (underlying connection = connected user)
-    def send_welcome_message(self, writer):
-        """
-        Sends a welcome message to a newly connected client
-        """
-        message = dedent(f"""
-        ===
-        Welcome {writer.nickname}!
-
-        There are {len(self.connection_pool) -1} user(s) here beside you
-
-        Help: 
-         - Type anything to chat
-         - /list will list all the connected users
-         - /quit will disconnect you
-        ===     
-        """)
-        writer.write(f"{message}\n".encode())
+        # using a dict now, mapping address to writer
+        self.connection_pool = dict() # represents peer connection
 
     def broadcast(self, writer, message):
         """
@@ -35,64 +20,29 @@ class ConnectionPool:
                 # we don't need to also broadcast to the user sending the message
                 user.write(f"{message}\n".encode())
 
-    def broadcast_user_join(self, writer):
-        """
-        Calls the broadcast method with a "user joining" message
-        """
-        self.broadcast(writer, f"{writer.nickname} just joined")
-
-    def broadcast_user_quit(self, writer):
-        """
-        Calls the broadcast method with a "user quitting" message
-        """
-        self.broadcast(writer, f"{writer.nickname} just quit")
-
-    def broadcast_new_message(self, writer, message):
-        """
-        Calls the broadcast method with a user's chat message
-        """
-        self.broadcast(writer, f"[{writer.nickname}] {message}")
-
-    def list_users(self, writer):
-        """
-        List all the users in the pool
-        """
-        message = "===\n"
-        message += "Currently connected users:"
-        for user in self.connection_pool:
-            if user == writer:
-                message += f"\n - {user.nickname} (you)"
-            else:
-                message += f"\n - {user.nickname}"
-        message += "\n==="
-        writer.write(f"{message}\n".encode())
-
-    def add_new_user_to_pool(self, writer):
-        """
-        Add a new user to our existing pool
-        """
-        self.connection_pool.add(writer)
-
-    def remove_user_from_pool(self, writer):
-        """
-        Removes an existing user from our existing pool
-        """
-        self.connection_pool.remove(writer)
-    
     @staticmethod
     def get_address_string(writer):
         # get a peer's ip:port (address)
-        pass
+        ip = writer.address["ip"]
+        port = writer.address["port"]
+        return f"{ip}:{port}" # address string in mapping is simply ip:port
+        # helps us to uniquely identify connections
 
     def add_peer(self, writer):
         # add a peer to our connection pool
-        pass
+        address = self.get_address_string(writer)
+        self.connection_pool[address] = writer
+        logger.info("Added new peer to pool", address=address)
 
     def remove_peer(self, writer):
         # remove a peer to our connection pool
-        pass
+        address = self.get_address_string(writer)
+        self.connection_pool.pop(address)
+        logger.info("Removed peer from pool", address=address)
 
     def get_alive_peers(self, count):
         # add a peer to our connection pool
-        pass
+        # TODO (Reader): Sort these by most active, but let's just get
+        # the first *count* of them for now
+        return take(count, self.connection_pool.items()) # using take() to return count number of peers
 
